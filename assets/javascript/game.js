48f925fb638dc73586fc2a23edaaaa97f818e0ec
Dominick Geronimo <lorddom22@gmail.com>
2019-02-01 01:56:11 -0500

    formatting and css reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,55 +10,55 @@ var gameVar = {
 
     userWins: 0,
     guessRemaining: 10,
-    wordList: ['overwatch', 'darksiders', 'slay the spire', 'nantucket', 'dead by daylight', 'total war', 'battletech'], 
+    wordList: ['overwatch', 'darksiders', 'slay the spire', 'nantucket', 'dead by daylight', 'total war', 'battletech'],
     answers: "",
     displayAnswer: [], //display correct letter and _
     incorrect: [], //display incorrect guess
     correct: [], //hold correct guesses so user can't repeat them
     active: false, //determines start text state and entry into game loop
 
- //Declare functions
- gameStart: function() {
-    this.guessRemaining = 10;
-    this.incorrect = [];
-    this.correct = [];
-    this.displayAnswer = [];
- //Pick word from array and load word into div _ _ _ _
-    var ranNum = Math.floor(Math.random() * this.wordList.length);
-    this.answers = this.wordList[ranNum];
-    this.wordUnderscore();
- },
+    //Declare functions
+    gameStart: function () {
+        this.guessRemaining = 10;
+        this.incorrect = [];
+        this.correct = [];
+        this.displayAnswer = [];
+        //Pick word from array and load word into div _ _ _ _
+        var ranNum = Math.floor(Math.random() * this.wordList.length);
+        this.answers = this.wordList[ranNum];
+        this.wordUnderscore();
+    },
 
- //Change word to underscores
-    wordUnderscore: function() { 
-        for (i = 0; i < this.answers.length; i++){
-            this.displayAnswer.push("_") 
+    //Change word to underscores
+    wordUnderscore: function () {
+        for (i = 0; i < this.answers.length; i++) {
+            this.displayAnswer.push("_")
 
-        } console.log(this.displayAnswer)
+        }
         docAnswer.textContent = this.displayAnswer.join(" ")
 
     }
 }
 // Player to press a key to begin
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
     var letter = event.key.toLowerCase();
-//If game is inactive: Begin game
+    //If game is inactive: Begin game
     if (gameVar.active === false) {
         gameVar.gameStart();
         gameVar.active = true;
     } //ToDo set else (if?) to get into game
 
 
-//First for loop to house the game
+    //First for loop to house the game
 
-//Second For loop checking guess against answer
+    //Second For loop checking guess against answer
 
-//if guess = letter in word then reveal letter
+    //if guess = letter in word then reveal letter
 
-//else if letter guessed = nothing
+    //else if letter guessed = nothing
 
-//else if deduct from guesses and add to guessed
+    //else if deduct from guesses and add to guessed
 
-//else wins ++ end loop
+    //else wins ++ end loop
 
 }
